abdbe19e647cfc40f347a5b095ea6c64067c3616
Nick Shiro <nikitakitsunedev@gmail.com>
2025-01-13 16:02:26 +0300

    Refactor panel size: remove js code and re-use youtube value

--- content-script/widgets/Panel/Panel.tsx
+++ content-script/widgets/Panel/Panel.tsx
@@ -1,5 +1,4 @@
 import copyLink from "./api/copyLink";
-import useResize from "@shared/hooks/useResize";
 import Button from "@shared/ui/Button/Button";
 import Expand from "@shared/ui/Expand/Expand";
 import Members from "@shared/ui/Members/Members";
@@ -9,8 +8,6 @@ import Playlist from "@widgets/Playlist/Playlist";
 import React, { useState } from "react";
 
 const Panel: React.FC = () => {
-    const { isFullScreen, height } = useResize();
-
     const [isExpanded, setIsExpanded] = useState(true);
     const [userCount, setUserCount] = useState<number>(0);
 
@@ -21,7 +18,7 @@ const Panel: React.FC = () => {
     return (
         <div
             className="st-panel m-0 box-border flex min-h-[400px] w-[100%] flex-col overflow-hidden rounded-[12px] border border-solid border-spec-outline"
-            style={{ height: height }}
+            style={{ height: "var(--ytd-watch-flexy-panel-max-height)" }}
         >
             <header className="m-0 box-border flex w-[100%]  items-center justify-between bg-background-primary border-b border-solid border-spec-outline border-t-0 border-l-0 border-r-0 p-[5px_6px_5px_12px]">
                 <p className="m-0 select-none p-0 font-primary text-[2rem] font-semibold leading-[2.8rem]  text-text-primary">
